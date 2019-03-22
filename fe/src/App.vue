84f4d9efc1ea5326e1d29fd7bb01438f89e5de14
Tamás Németh <tomuwhu@gmail.com>
2019-03-22 10:33:39 +0100

    .

--- fe/src/App.vue
+++ fe/src/App.vue
@@ -1,20 +1,39 @@
 <template>
   <div id="app">
-    Cica {{v}}
-    <hr>
-    <button @click="v++">+</button>
-    <button @click="v--">-</button>
+    <table>
+      <tr :key="i" v-for="(row,i) in t">
+        <td :key="j" v-for="(cell,j) in row">
+          {{cell}}
+        </td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
 export default {
   name: 'app',
-  data: () => ({
-    v: 1
-  })
+  data() {
+    return {
+      t: Array(20)
+        .fill(0)
+        .map( () => 
+          Array(20)
+            .fill(0)
+            .map( () => "" )
+        )
+    }
+  }
 }
 </script>
 
 <style>
+ td {
+   width:15px;
+   height:15px;
+   border-radius: 3px;
+   background-color: aqua ;
+   box-shadow: 1px 1px 4px black;
+   cursor:pointer;
+ }
 </style>
