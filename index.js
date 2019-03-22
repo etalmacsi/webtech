f6cb84c7343555021a6135c83dc7082d25dfbc39
Tamás Németh <tomuwhu@gmail.com>
2019-03-22 11:20:55 +0100

    .

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ const express=require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const app = express()
+const MongoClient = require('mongodb').MongoClient
+var db
 app.use(cors())
 app.use(bodyParser.json())
+MongoClient.connect('mongodb://localhost:27017/', {
+            useNewUrlParser: true
+        }, (err, client) => {
+    if (err) throw err
+    db = client.db('legyen')
+})
+
 
 app.get('/', 
     (req, res) => 
@@ -13,7 +22,9 @@ app.get('/',
 app.post('/',
     (req, res) => {
         console.log(req.body)
-        res.send('Britney')
+        db.collection('mammals').insertOne(req.body, cucc => {
+            res.send( cucc )
+        })
     }
 )
 
