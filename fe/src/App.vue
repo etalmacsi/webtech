984e977ceaed8de9c388920e1c7d20778368bc83
Tamás Németh <tomuwhu@gmail.com>
2019-03-22 10:13:31 +0100

    .

--- fe/src/App.vue
+++ fe/src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
-    Cica
+    Cica {{v}}
+    <hr>
+    <button @click="v++">+</button>
+    <button @click="v--">-</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data: () => ({
+    v: 1
+  })
 }
 </script>
 
